cf5a4cbc95759fcbedf101e715d898bd4804fc0d
Graham Ollis <plicease@cpan.org>
2022-09-06 10:56:58 -0600

    bare bones

--- lib/Alien/Build/Plugin/Download/GitLab.pm
+++ lib/Alien/Build/Plugin/Download/GitLab.pm
@@ -3,9 +3,15 @@ package Alien::Build::Plugin::Download::GitLab;
 use strict;
 use warnings;
 use 5.008004;
+use Alien::Build::Plugin;
 
 # ABSTRACT: Alien::Build plugin to download from GitLab
 # VERSION
+
+sub init
+{
+  my($self, $meta) = @_;
+}
 
 1;
 
